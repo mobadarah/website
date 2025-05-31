1acbb06402313be7808faf1092cc402da270a464
abdo-spices <spices.digital@gmail.com>
2025-05-31 23:13:46 +0300

    feat: enhance Astro configuration with local font support and update package.json for biome

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: "local",
+				name: "masmak",
+				cssVariable: "--font-masmak",
+				variants: [
+					{
+						display: "swap",
+						weight: 700,
+						style: "normal",
+						src: ["./src/assets/fonts/MasmakBHD.woff2"],
+					},
+				],
+			},
+		],
+	},
 });
